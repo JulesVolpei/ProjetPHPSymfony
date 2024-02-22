07027eea2e28569f4413461fcabac70ac5314ffe
GONTIER Luke <luke.gontier@etu.univ-amu.fr>
2024-02-22 09:19:56 +0100

    - JQuery + example js

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,8 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.css'; 
+import './js/jquery-3.7.1.min.js';
+import './js/random.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
