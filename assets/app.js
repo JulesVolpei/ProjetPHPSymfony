07a77378a9f08e5087e6f3cd2abeaaaae1ee9c6e
JulesVolpei <jules.volpei@etu.univ-amu.fr>
2024-02-22 09:36:46 +0100

    Changement route CSS dans app.js

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,29 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/userLogin.css';
+import './styles/registration.css';
+/*
+import './js/jquery-3.7.1.min.js';
+import './js/random.js';
+*/
+// Ajouter les routes vers les nouveaux styles
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+var titre = document.title;
+console.log(titre);
+console.log(document.getElementById('css').getAttribute('href'));
+
+switch (titre) {
+    case 'Hello HomePageController!':
+        document.getElementById('css').setAttribute('href', './styles/app.css');
+        break;
+    case 'Log in!':
+        document.getElementById('css').setAttribute('href', './styles/userLogin.css');
+        break;
+    case 'Création de compte':
+        document.getElementById('css').setAttribute('href', './styles/registration.css');
+        break;
+    // Ajouter un case pour chaque nouvelle page
+}
